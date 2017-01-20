1e17f3b9cb99bce734ef792dbcdb655f9fb17ab4
Atsuhiro Nabeta <nabeta-a@legendapl.com>
2017-01-20 18:18:14 +0900

    [REFACTOR] Rename ExternalStorageModule into AndroidModule

--- app/src/main/java/com/lakeel/altla/vision/admanager/presentation/di/component/ApplicationComponent.java
+++ app/src/main/java/com/lakeel/altla/vision/admanager/presentation/di/component/ApplicationComponent.java
@@ -1,9 +1,9 @@
 package com.lakeel.altla.vision.admanager.presentation.di.component;
 
 import com.lakeel.altla.vision.admanager.presentation.di.module.ActivityModule;
+import com.lakeel.altla.vision.admanager.presentation.di.module.AndroidModule;
 import com.lakeel.altla.vision.admanager.presentation.di.module.ApplicationModule;
 import com.lakeel.altla.vision.admanager.presentation.di.module.ConfigModule;
-import com.lakeel.altla.vision.admanager.presentation.di.module.ExternalStorageModule;
 import com.lakeel.altla.vision.admanager.presentation.di.module.FirebaseModule;
 import com.lakeel.altla.vision.admanager.presentation.di.module.GoogleSignInModule;
 
@@ -16,7 +16,7 @@ import dagger.Component;
 @Singleton
 @Component(modules = { ApplicationModule.class,
                        ConfigModule.class,
-                       ExternalStorageModule.class,
+                       AndroidModule.class,
                        GoogleSignInModule.class,
                        FirebaseModule.class })
 public interface ApplicationComponent {
