f5d8068d829752fc8ef81562d148776cc45fd3ff
Atsuhiro Nabeta <nabeta-a@legendapl.com>
2017-01-23 15:49:05 +0900

    [UPDATE] Show the user profile on the drawer

--- app/src/main/java/com/lakeel/altla/vision/admanager/presentation/di/module/FirebaseRepositoryModule.java
+++ app/src/main/java/com/lakeel/altla/vision/admanager/presentation/di/module/FirebaseRepositoryModule.java
@@ -3,8 +3,10 @@ package com.lakeel.altla.vision.admanager.presentation.di.module;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 
+import com.lakeel.altla.vision.data.repository.firebase.ConnectionRepository;
 import com.lakeel.altla.vision.data.repository.firebase.UserAreaDescriptionFileRepository;
 import com.lakeel.altla.vision.data.repository.firebase.UserAreaDescriptionRepository;
+import com.lakeel.altla.vision.data.repository.firebase.UserConnectionRepository;
 import com.lakeel.altla.vision.data.repository.firebase.UserDeviceRepository;
 import com.lakeel.altla.vision.data.repository.firebase.UserProfileRepository;
 import com.lakeel.altla.vision.di.ActivityScope;
@@ -14,6 +16,18 @@ import dagger.Provides;
 
 @Module
 public final class FirebaseRepositoryModule {
+
+    @ActivityScope
+    @Provides
+    public ConnectionRepository provideConnectionRepository(FirebaseDatabase database) {
+        return new ConnectionRepository(database);
+    }
+
+    @ActivityScope
+    @Provides
+    public UserConnectionRepository provideUserConnectionRepository(FirebaseDatabase database) {
+        return new UserConnectionRepository(database);
+    }
 
     @ActivityScope
     @Provides
--- app/src/main/java/com/lakeel/altla/vision/admanager/presentation/view/activity/MainActivity.java
+++ app/src/main/java/com/lakeel/altla/vision/admanager/presentation/view/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.lakeel.altla.vision.admanager.presentation.view.activity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import com.lakeel.altla.tango.TangoWrapper;
 import com.lakeel.altla.vision.admanager.R;
@@ -13,8 +14,14 @@ import com.lakeel.altla.vision.admanager.presentation.view.fragment.EditUserArea
 import com.lakeel.altla.vision.admanager.presentation.view.fragment.SignInFragment;
 import com.lakeel.altla.vision.admanager.presentation.view.fragment.TangoPermissionFragment;
 import com.lakeel.altla.vision.admanager.presentation.view.fragment.TangoSpaceFragment;
+import com.lakeel.altla.vision.domain.usecase.ObserveConnectionUseCase;
+import com.lakeel.altla.vision.domain.usecase.ObserveUserProfileUseCase;
+import com.lakeel.altla.vision.domain.usecase.SignOutUseCase;
+import com.squareup.picasso.Picasso;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -23,11 +30,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
 
 public final class MainActivity extends AppCompatActivity
         implements ActivityScopeContext,
@@ -43,6 +55,14 @@ public final class MainActivity extends AppCompatActivity
     @Inject
     TangoWrapper tangoWrapper;
 
+    @Inject
+    ObserveUserProfileUseCase observeUserProfileUseCase;
+
+    @Inject
+    ObserveConnectionUseCase observeConnectionUseCase;
+
+    @Inject
+    SignOutUseCase signOutUseCase;
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
@@ -53,9 +73,17 @@ public final class MainActivity extends AppCompatActivity
     @BindView(R.id.navigation_view)
     NavigationView navigationView;
 
+    private final CompositeDisposable compositeDisposable = new CompositeDisposable();
+
     private final FragmentController fragmentController = new FragmentController();
 
     private ActivityComponent activityComponent;
+
+    private NavigationViewHeader navigationViewHeader;
+
+    private Disposable observeUserProfileDisposable;
+
+    private Disposable observeConnectionDisposable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,8 +106,36 @@ public final class MainActivity extends AppCompatActivity
         toggle.syncState();
 
         navigationView.setNavigationItemSelectedListener(this);
+        navigationViewHeader = new NavigationViewHeader(navigationView);
 
         showSignInFragment();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseAuth.getInstance().addAuthStateListener(navigationViewHeader);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        // Unsubscribe the connection.
+        if (observeConnectionDisposable != null) {
+            observeConnectionDisposable.dispose();
+            observeConnectionDisposable = null;
+        }
+
+        // Unsubscribe the user profile.
+        if (observeUserProfileDisposable != null) {
+            observeUserProfileDisposable.dispose();
+            observeUserProfileDisposable = null;
+        }
+
+        compositeDisposable.clear();
+
+        FirebaseAuth.getInstance().removeAuthStateListener(navigationViewHeader);
     }
 
     @Override
@@ -137,8 +193,7 @@ public final class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_app_space) {
             fragmentController.showAppSpaceFragment();
         } else if (id == R.id.nav_sign_out) {
-            FirebaseAuth.getInstance().signOut();
-            showSignInFragment();
+            onSignOut();
         }
 
         drawerLayout.closeDrawer(GravityCompat.START);
@@ -163,6 +218,14 @@ public final class MainActivity extends AppCompatActivity
     @Override
     public void onCloseTangoPermissionFragment() {
         fragmentController.showTangoSpaceFragment();
+    }
+
+    private void onSignOut() {
+        Disposable disposable = signOutUseCase
+                .execute()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this::showSignInFragment);
+        compositeDisposable.add(disposable);
     }
 
     private void showSignInFragment() {
@@ -214,6 +277,70 @@ public final class MainActivity extends AppCompatActivity
                 getSupportFragmentManager().beginTransaction()
                                            .replace(R.id.fragment_container, fragment, FRAGMENT_TAG_APP_SPACE)
                                            .commit();
+            }
+        }
+    }
+
+    class NavigationViewHeader implements FirebaseAuth.AuthStateListener {
+
+        @BindView(R.id.image_view_user_photo)
+        ImageView imageViewUserPhoto;
+
+        @BindView(R.id.text_view_user_name)
+        TextView textViewUserName;
+
+        @BindView(R.id.text_view_user_email)
+        TextView textViewUserEmail;
+
+        private NavigationViewHeader(@NonNull NavigationView navigationView) {
+            ButterKnife.bind(this, navigationView.getHeaderView(0));
+        }
+
+        @Override
+        public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+            FirebaseUser user = firebaseAuth.getCurrentUser();
+
+            if (user != null) {
+                // Subscribe the connection.
+                if (observeConnectionDisposable == null) {
+                    observeConnectionDisposable = observeConnectionUseCase
+                            .execute()
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .subscribe();
+                }
+
+                // Subscribe the user profile.
+                if (observeUserProfileDisposable == null) {
+                    observeUserProfileDisposable = observeUserProfileUseCase
+                            .execute()
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .subscribe(userProfile -> {
+                                // Update UI each time the user profile is updated.
+                                if (userProfile.photoUri != null) {
+                                    Uri photoUri = Uri.parse(userProfile.photoUri);
+                                    Picasso.with(MainActivity.this).load(photoUri).into(imageViewUserPhoto);
+                                }
+                                textViewUserName.setText(userProfile.displayName);
+                                textViewUserEmail.setText(userProfile.email);
+                            });
+                }
+            } else {
+                // Unsubscribe the connection.
+                if (observeConnectionDisposable != null) {
+                    observeConnectionDisposable.dispose();
+                    observeConnectionDisposable = null;
+                }
+
+                // Unsubscribe the user profile.
+                if (observeUserProfileDisposable != null) {
+                    observeUserProfileDisposable.dispose();
+                    observeUserProfileDisposable = null;
+                }
+
+                // Clear UI.
+                imageViewUserPhoto.setImageBitmap(null);
+                textViewUserName.setText(null);
+                textViewUserEmail.setText(null);
             }
         }
     }
