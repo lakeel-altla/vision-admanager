340f5e42937dc5a823a44354f91fc7dc5550b8ba
Atsuhiro Nabeta <nabeta-a@legendapl.com>
2017-02-06 21:44:58 +0900

    [UPDATE] Display createdAt value on the view

--- app/src/main/java/com/lakeel/altla/vision/admanager/presentation/view/fragment/UserAreaDescriptionFragment.java
+++ app/src/main/java/com/lakeel/altla/vision/admanager/presentation/view/fragment/UserAreaDescriptionFragment.java
@@ -17,6 +17,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
 import android.support.design.widget.Snackbar;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -45,6 +46,8 @@ public final class UserAreaDescriptionFragment
     @BindView(R.id.text_view_id)
     TextView textViewId;
 
+    @BindView(R.id.text_view_created_at)
+    TextView textViewCreatedAt;
 
     @BindView(R.id.text_view_name)
     TextView textViewName;
@@ -174,6 +177,12 @@ public final class UserAreaDescriptionFragment
     @Override
     public void onModelUpdated(@NonNull UserAreaDescriptionModel model) {
         textViewId.setText(model.areaDescriptionId);
+        String createdAtString = null;
+        if (0 < model.createdAt) {
+            createdAtString = DateFormat.getDateFormat(getContext()).format(model.createdAt) + " " +
+                              DateFormat.getTimeFormat(getContext()).format(model.createdAt);
+        }
+        textViewCreatedAt.setText(createdAtString);
         textViewName.setText(model.name);
         textViewAreaName.setText(model.areaName);
 
